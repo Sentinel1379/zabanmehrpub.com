123a301b45c6b438ef43351c59b6b75f5340176a
Sentinel1379 <sadeghanalooee31@gmail.com>
2024-02-10 12:51:03 +0330

    second commit

---
+++ data.js
@@ -0,0 +1,284 @@
+import './db.json'
+{
+    "card": [
+        {
+            "id": 1,
+            "imgTag": "-55%",
+            "imgSrc": "https://zabanmehrpub.com/wp-content/uploads/Oxford-word-skills-2-e1623755978661-300x300.jpg",
+            "imgAlt": "کتاب Oxford Word Skills",
+            "cardName": "مجموعه کتاب های Oxford Word Skills ویرایش 1",
+            "rating": 5,
+            "Available": "موجود",
+            "Preprice": "510000",
+            "Newprice": "229500",
+            "print": "چاپ A+",
+            "bookCode": "ZM-1927"
+        },
+        {
+            "id": 2,
+            "imgTag": "-50%",
+            "imgSrc": "https://zabanmehrpub.com/wp-content/uploads/vocabulary-for-ielts-2-300x300.jpg",
+            "imgAlt": "کتاب Oxford Word Skills",
+            "cardName": "Vocabulary for IELTS",
+            "rating": 5,
+            "Available": "موجود",
+            "Preprice": "135000",
+            "Newprice": "67500",
+            "print": "چاپ A+",
+            "bookCode": "ZM-1299"
+        },
+        {
+            "id": 3,
+            "imgTag": "-50%",
+            "imgSrc": "https://zabanmehrpub.com/wp-content/uploads/2020/05/Oxford-Word-Skills-Basic-500x500.webp",
+            "imgAlt": "کتاب Oxford word skills basics",
+            "cardName": "Oxford Word Skills Basic",
+            "rating": 5,
+            "Available": "موجود",
+            "Preprice": "135000",
+            "Newprice": "85000",
+            "print": "چاپ A+",
+            "bookCode": "ZM-2268"
+        },
+        {
+            "id": 4,
+            "imgTag": "-55%",
+            "imgSrc": "https://zabanmehrpub.com/wp-content/uploads/2020/01/آکادمیک-11-تا-18-500x500.jpg",
+            "imgAlt": "خرید کتاب زبان خارجه با 50% تخفیف ویژه ❤️ – زبانمهر",
+            "cardName": "Cambridge IELTS Academic 11-18 Book Series",
+            "rating": 5,
+            "Available": "موجود",
+            "Preprice": "800000",
+            "Newprice": "360000",
+            "print": "چاپ A+",
+            "bookCode": "ZM-2008"
+        },
+        {
+            "id": 5,
+            "imgTag": "-55%",
+            "imgSrc": "https://zabanmehrpub.com/wp-content/uploads/2021/05/مجموعه-کتاب-های-Oxford-Word-Skills-Second-Edition-2-500x500.jpg",
+            "imgAlt": "خرید کتاب زبان خارجه با 50% تخفیف ویژه ❤️ – زبانمهر",
+            "cardName": "مجموعه کتاب های Oxford Word Skills Second Edition",
+            "rating": 5,
+            "Available": "موجود",
+            "Preprice": "660000",
+            "Newprice": "330000",
+            "print": "چاپ A+",
+            "bookCode": "ZM-2957"
+        },
+        {
+            "id": 6,
+            "imgTag": "-60%",
+            "imgSrc": "https://zabanmehrpub.com/wp-content/uploads/2023/10/the-wager-500x500.jpg",
+            "imgAlt": "خرید کتاب زبان خارجه با 50% تخفیف ویژه ❤️ – زبانمهر",
+            "cardName": "The Wager",
+            "rating": 0,
+            "Available": "موجود",
+            "Preprice": "300000",
+            "Newprice": "120000",
+            "print": "چاپ دیجیتال",
+            "bookCode": "ZM-3375860"
+        },
+        {
+            "id": 7,
+            "imgTag": "-40%",
+            "imgSrc": "https://zabanmehrpub.com/wp-content/uploads/2023/10/Speakout-C1.C2-Third-Edition-500x500.jpg",
+            "imgAlt": "خرید کتاب زبان خارجه با 50% تخفیف ویژه ❤️ – زبانمهر",
+            "cardName": "Speakout C1.C2 Third Edition",
+            "rating": 0,
+            "Available": "موجود",
+            "Preprice": "700000",
+            "Newprice": "420000",
+            "print": "چاپ دیجیتال",
+            "bookCode": "ZM-3375859"
+        },
+        {
+            "id": 8,
+            "imgTag": "-40%",
+            "imgSrc": "https://zabanmehrpub.com/wp-content/uploads/2023/10/Speakout-B2-Third-Edition-500x500.jpg",
+            "imgAlt": "خرید کتاب زبان خارجه با 50% تخفیف ویژه ❤️ – زبانمهر",
+            "cardName": "Speakout B2 Third Edition",
+            "rating": 0,
+            "Available": "موجود",
+            "Preprice": "700000",
+            "Newprice": "420000",
+            "print": "چاپ دیجیتال",
+            "bookCode": "ZM-3375858"
+        },
+        {
+            "id": 9,
+            "imgTag": "-40%",
+            "imgSrc": "https://zabanmehrpub.com/wp-content/uploads/2023/10/Speakout-B1-Third-Edition-500x500.jpg",
+            "imgAlt": "خرید کتاب زبان خارجه با 50% تخفیف ویژه ❤️ – زبانمهر",
+            "cardName": "Speakout B1 Third Edition",
+            "rating": 0,
+            "Available": "موجود",
+            "Preprice": "700000",
+            "Newprice": "420000",
+            "print": "چاپ دیجیتال",
+            "bookCode": "ZM-337584"
+        },
+        {
+            "id": 10,
+            "imgTag": "-40%",
+            "imgSrc": "https://zabanmehrpub.com/wp-content/uploads/2023/10/Speakout-A2-Third-Edition-500x500.jpg",
+            "imgAlt": "خرید کتاب زبان خارجه با 50% تخفیف ویژه ❤️ – زبانمهر",
+            "cardName": "Speakout A2 Third Edition",
+            "rating": 0,
+            "Available": "موجود",
+            "Preprice": "700000",
+            "Newprice": "420000",
+            "print": "چاپ دیجیتال",
+            "bookCode": "ZM-337583"
+        },
+        {
+            "id": 11,
+            "imgTag": "-30%",
+            "imgSrc": "https://zabanmehrpub.com/wp-content/uploads/2019/09/سوپر-پک-عید5-300x300.jpg",
+            "imgAlt": "سوپرپک کتاب‌های جان گرین",
+            "cardName": "سوپرپک کتاب‌های جان گرین",
+            "rating": 4,
+            "Available": "موجود",
+            "Preprice": "630000",
+            "Newprice": "252000",
+            "print": "چاپ A+",
+            "bookCode": "ZM-1894"
+        },
+        {
+            "id": 12,
+            "imgTag": "-40%",
+            "imgSrc": "https://zabanmehrpub.com/wp-content/uploads/2018/10/فلش-کارت-504-300x300.jpg",
+            "imgAlt": "خرید کتاب زبان خارجه با 50% تخفیف ویژه ❤️ – زبانمهر",
+            "cardName": "فلش کارت 504 واژه کاملاً ضروری انگلیسی",
+            "rating": 5,
+            "Available": "موجود",
+            "Preprice": "190000",
+            "Newprice": "114000",
+            "print": "چاپ A+",
+            "bookCode": "ZM-1031"
+        },
+        {
+            "id": 13,
+            "imgTag": "-40%",
+            "imgSrc": "https://zabanmehrpub.com/wp-content/uploads/2024/01/The-Wicked-King-500x500.jpg",
+            "imgAlt": "The Wicked King",
+            "cardName": "The Wicked King",
+            "rating": 0,
+            "Available": "موجود",
+            "Preprice": "220000",
+            "Newprice": "880000",
+            "print": "چاپ A+",
+            "bookCode": "ZM-3375998"
+        },
+        {
+            "id": 14,
+            "imgTag": "-60%",
+            "imgSrc": "https://zabanmehrpub.com/wp-content/uploads/2024/01/learning-to-love-myself-500x500.jpg",
+            "imgAlt": "Learning To Love Myself",
+            "cardName": "Learning To Love Myself",
+            "rating": 5,
+            "Available": "موجود",
+            "Preprice": "150000",
+            "Newprice": "60000",
+            "print": "چاپ A+",
+            "bookCode": "ZM-3375997"
+        },
+        {
+            "id": 15,
+            "imgTag": "-60%",
+            "imgSrc": "https://zabanmehrpub.com/wp-content/uploads/2023/02/4-300x300.jpg",
+            "imgAlt": "خرید کتاب زبان خارجه با 50% تخفیف ویژه ❤️ – زبانمهر",
+            "cardName": "سوپر پک عاشقانه پر از کشمکش",
+            "rating": 0,
+            "Available": "موجود",
+            "Preprice": "650000",
+            "Newprice": "260000",
+            "print": "چاپ A+",
+            "bookCode": "ZM-33636"
+        },
+        {
+            "id": 16,
+            "imgTag": "-30%",
+            "imgSrc": "https://zabanmehrpub.com/wp-content/uploads/Ielts-in-10-Day-300x300.jpg",
+            "imgAlt": "Ielts-in-10-Day",
+            "cardName": "کتاب لغات پیشرفته آیلتس در 10 روز",
+            "rating": 5,
+            "Available": "موجود",
+            "Preprice": "150000",
+            "Newprice": "105000",
+            "print": "چاپ A+",
+            "bookCode": "ZM-1014"
+        },
+        {
+            "id": 17,
+            "imgTag": "-50%",
+            "imgSrc": "https://zabanmehrpub.com/wp-content/uploads/ielts-Cambridge-1-300x300.jpg",
+            "imgAlt": "ielts-Cambridge-1",
+            "cardName": "Cambridge IELTS 1",
+            "rating": 0,
+            "Available": "موجود",
+            "Preprice": "110000",
+            "Newprice": "55000",
+            "print": "چاپ A+",
+            "bookCode": "ZM-1159"
+        },
+        {
+            "id": 18,
+            "imgTag": "-50%",
+            "imgSrc": "https://zabanmehrpub.com/wp-content/uploads/ielts-2-300x300.jpg",
+            "imgAlt": "Cambridge-IELTS-2",
+            "cardName": "Cambridge IELTS 2",
+            "rating": 0,
+            "Available": "موجود",
+            "Preprice": "110000",
+            "Newprice": "55000",
+            "print": "چاپ A+",
+            "bookCode": "ZM-1160"
+        },
+        {
+            "id": 19,
+            "imgTag": "-50%",
+            "imgSrc": "https://zabanmehrpub.com/wp-content/uploads/2018/11/3-500x500.jpg",
+            "imgAlt": "خرید کتاب زبان خارجه با 50% تخفیف ویژه ❤️ – زبانمهر",
+            "cardName": "Cambridge IELTS 3",
+            "rating": 5,
+            "Available": "موجود",
+            "Preprice": "110000",
+            "Newprice": "55000",
+            "print": "چاپ A+",
+            "bookCode": "ZM-1161"
+        },
+        {
+            "id": 20,
+            "imgTag": "-50%",
+            "imgSrc": "https://zabanmehrpub.com/wp-content/uploads/ielts-4-300x300.jpg",
+            "imgAlt": "Cambridge-IELTS-4",
+            "cardName": "Cambridge IELTS 4",
+            "rating": 0,
+            "Available": "موجود",
+            "Preprice": "110000",
+            "Newprice": "55000",
+            "print": "چاپ A+",
+            "bookCode": "ZM-1162"
+        }
+    ]
+}
+
+const data = require('./db.json');
+const { json } = require('micro');
+
+module.exports = async (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+
+    if (req.method === 'OPTIONS') {
+        return res.status(200).end();
+    }
+
+    if (req.method === 'GET') {
+        return res.status(200).json(data);
+    }
+
+    return res.status(405).end('Method Not Allowed');
+}
